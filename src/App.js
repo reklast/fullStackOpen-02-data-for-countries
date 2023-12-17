ce78ac041bea363dbefe4bc1a353d84935ce04b9
reklast <reklast51@gmail.com>
2023-12-17 18:49:11 +0200

    dependencies update and solution implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Choice from './components/Choice'
 
-function App() {
+
+
+const App = () => {
+
+  const [country, setCountry] = useState('')
+  const [result, setResult] = useState([])
+
+
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        setResult(response.data)
+      })
+  }, [])
+
+
+  const handleCountryChange = (event) => {
+    setCountry(event.target.value)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        find countries <input value={country} onChange={handleCountryChange} />
+      </form>
+      <div>
+        <Choice key={result.id} result={result} country={country} />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
